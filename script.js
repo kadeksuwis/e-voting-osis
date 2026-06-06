92f7bf52fe2d55771c9584420056bf198faf43fc
kadeksuwis <suwis.techy@gmail.com>
2026-06-06 13:41:45 +0800

    Update tampilan welcome dan integrasi spreadsheet

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let pilihanSekarang = null;
 
 // GANTI NANTI DENGAN URL APPS SCRIPT
-const API_URL = "";
+const API_URL = "https://script.google.com/macros/s/AKfycbyJO3T9iV2EdGu8AaFxR8_GSbe_K3PiW1m8iLQ8kA9QbKIoCvsGyq6ogtvOV3TQZhduEA/exec";
 
 function mulaiVoting(){
 
@@ -75,15 +75,27 @@ document.getElementById("btnYa").onclick = function(){
 
     document.getElementById("confirmModal").style.display = "none";
 
-    // nanti kirim ke spreadsheet
+    fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify({
+            paslon: pilihanSekarang
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
 
-    console.log(
-        "Vote untuk Paslon",
-        pilihanSekarang
-    );
+        console.log("Vote berhasil:", data);
 
-    document.getElementById("voting").style.display = "none";
+        document.getElementById("voting").style.display = "none";
+        document.getElementById("terimakasih").style.display = "flex";
 
-    document.getElementById("terimakasih").style.display = "flex";
+    })
+    .catch(error => {
+
+        console.error("Error:", error);
+
+        alert("Gagal mengirim suara ke Spreadsheet");
+
+    });
 
 }
