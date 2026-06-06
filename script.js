3108f7d3dc89d7a59eac5802392cd96e22d57406
kadeksuwis <suwis.techy@gmail.com>
2026-06-06 13:56:47 +0800

    Fix spreadsheet

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let pilihanSekarang = null;
 
-// GANTI NANTI DENGAN URL APPS SCRIPT
+// URL Apps Script
 const API_URL =
   "https://script.google.com/macros/s/AKfycbyJO3T9iV2EdGu8AaFxR8_GSbe_K3PiW1m8iLQ8kA9QbKIoCvsGyq6ogtvOV3TQZhduEA/exec";
 
@@ -15,7 +15,6 @@ function detailPaslon(nomor) {
 
   if (nomor == 1) {
     judul = "Paslon 1";
-
     isi =
       "VISI:\nOSIS aktif dan inovatif.\n\n" +
       "MISI:\n- Menambah kegiatan siswa\n- Meningkatkan disiplin";
@@ -23,7 +22,6 @@ function detailPaslon(nomor) {
 
   if (nomor == 2) {
     judul = "Paslon 2";
-
     isi =
       "VISI:\nMenciptakan sekolah kreatif.\n\n" +
       "MISI:\n- Lomba rutin\n- Pengembangan bakat";
@@ -31,7 +29,6 @@ function detailPaslon(nomor) {
 
   if (nomor == 3) {
     judul = "Paslon 3";
-
     isi =
       "VISI:\nSekolah berprestasi.\n\n" +
       "MISI:\n- Pembinaan akademik\n- Pengembangan karakter";
@@ -49,7 +46,6 @@ function tutupDetail() {
 
 function pilih(nomor) {
   pilihanSekarang = nomor;
-
   document.getElementById("confirmModal").style.display = "flex";
 }
 
@@ -58,24 +54,33 @@ function tutupKonfirmasi() {
 }
 
 document.getElementById("btnYa").onclick = function () {
+
   document.getElementById("confirmModal").style.display = "none";
 
   fetch(API_URL, {
     method: "POST",
     mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({
-      paslon: pilihanSekarang,
-    }),
-  })
-    .then(() => {
-      console.log("Vote berhasil:", data);
-
-      document.getElementById("voting").style.display = "none";
-      document.getElementById("terimakasih").style.display = "flex";
+      paslon: pilihanSekarang
     })
-    .catch((error) => {
-      console.error("Error:", error);
+  })
+  .then(() => {
 
-      alert("Gagal mengirim suara ke Spreadsheet");
-    });
+    console.log("Vote berhasil dikirim");
+
+    document.getElementById("voting").style.display = "none";
+    document.getElementById("terimakasih").style.display = "flex";
+
+  })
+  .catch((error) => {
+
+    console.error(error);
+
+    alert("Gagal mengirim suara ke Spreadsheet");
+
+  });
+
 };
